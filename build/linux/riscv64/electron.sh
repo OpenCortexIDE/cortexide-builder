c49f2797887e246d08f244e54005d66831ae1836
Tajudeen <pterjudinouin@outlook.com>
2025-12-29 15:29:35 +0000

    Fix: Update riscv64 Electron version to 37.10.3 in electron.sh to match package_bin.sh

--- build/linux/riscv64/electron.sh
+++ build/linux/riscv64/electron.sh
@@ -2,5 +2,6 @@
 
 set -ex
 
-export ELECTRON_VERSION="37.7.0"
+# Use 37.10.3 as v37.7.0 may not be available in riscv-forks/electron-riscv-releases
+export ELECTRON_VERSION="37.10.3"
 export VSCODE_ELECTRON_TAG="v${ELECTRON_VERSION}.riscv1"
